ddfb9b98751ecce39107c1ea3aae86de568bc7b0
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-02-16 15:06:10 +0200

    Fix issues.

--- slider/__init__.py
+++ slider/__init__.py
@@ -5,17 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 
 def setup_settings(settings, is_prod, **kwargs):
 
-    settings['INSTALLED_APPS'] += [
-        app for app in [
-            'faq',
-        ] if app not in settings['INSTALLED_APPS']
-    ]
+    if 'ordered_model' not in settings['INSTALLED_APPS']:
+        settings['INSTALLED_APPS'] += ['ordered_model']
 
-    settings['STYLESHEETS'] += [
-        app for app in [
-            'slider/slideshow.css',
-        ]
-    ]
+    settings['STYLESHEETS'] += ['slider/slideshow.css']
 
     settings['STATIC_APPS'] += [
         app for app in [
